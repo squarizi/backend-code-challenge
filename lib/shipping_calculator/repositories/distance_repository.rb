d5a4d637f3299214909b6cceb09163bbde3e8a56
squarizi <fesqua@yahoo.com.br>
2019-11-01 11:24:26 -0300

    Creating existence verification of origin or destination in Distance repository

--- lib/shipping_calculator/repositories/distance_repository.rb
+++ lib/shipping_calculator/repositories/distance_repository.rb
@@ -2,4 +2,10 @@ class DistanceRepository < Hanami::Repository
   def find_id_by_origin_and_destination(origin, destination)
     distances.select(:id).where(origin: origin, destination: destination).one&.id
   end
+
+  def origin_or_destination_exist?(origin, destination)
+    sql = Sequel.lit("origin = ? or destination = ?", origin, destination)
+
+    distances.select(:id).where(sql).exist?
+  end
 end
--- spec/shipping_calculator/repositories/distance_repository_spec.rb
+++ spec/shipping_calculator/repositories/distance_repository_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe DistanceRepository, type: :repository do
       it { expect(repository.find_id_by_origin_and_destination(origin, destination)).to be_nil}
     end
 
-    context 'when distance does not exist' do
+    context 'when distance exist' do
       let(:time) { Time.now.to_i % 6_000 }
       let(:origin) { "A #{time}" }
       let(:destination) { "B #{time}" }
@@ -19,6 +19,29 @@ RSpec.describe DistanceRepository, type: :repository do
       before { distance }
 
       it { expect(repository.find_id_by_origin_and_destination(origin, destination)).to eq distance.id }
+    end
+  end
+
+  describe '#exist_origin_or_destination?' do
+    let(:repository) { DistanceRepository.new }
+
+    context 'when origin or destination does not exist' do
+      let(:origin) { 'origin' }
+      let(:destination) { 'destination' }
+
+      it { expect(repository.origin_or_destination_exist?(origin, destination)).to be_falsey}
+    end
+
+    context 'when origin or destination exist' do
+      let(:time) { Time.now.to_i % 7_000 }
+      let(:origin) { "A #{time}" }
+      let(:destination) { "B #{time}" }
+      let(:kilometers) { rand(1..10000) }
+      let(:distance) { repository.create(origin: origin, destination: destination, kilometers: kilometers) }
+
+      before { distance }
+
+      it { expect(repository.origin_or_destination_exist?(origin, destination)).to be_truthy }
     end
   end
 end
